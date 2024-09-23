e0ff3f5ab1cdf9716d03b9a7b401e7ca5d6be60b
Observer3510 <missjiayiwu@gmail.com>
2024-09-23 14:44:24 -0400

    Finished LanguageCodeConverter

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -13,8 +13,8 @@ import java.util.Map;
  */
 public class LanguageCodeConverter {
     private int numLanguages;
-    private String languageCode;
-    private String languageName;
+    private List<String> languageCode;
+    private List<String> languageName;
     // Task: pick appropriate instance variables to store the data necessary for this class
 
     /**
@@ -36,8 +36,19 @@ public class LanguageCodeConverter {
             List<String> lines = Files.readAllLines(Paths.get(getClass()
                     .getClassLoader().getResource(filename).toURI()));
 
-            // TODO Task: use lines to populate the instance variable
+            //  Task: use lines to populate the instance variable
             //           tip: you might find it convenient to create an iterator using lines.iterator()
+            // remove the header
+            lines.remove(0);
+            this.numLanguages = lines.size();
+
+            String[] lineContent;
+            for (String line : lines) {
+                lineContent = line.split("\t");
+                this.languageCode.add(lineContent[1]);
+                this.languageName.add(lineContent[0]);
+
+            }
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
@@ -51,8 +62,9 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return code;
+        int nameIndex = this.languageCode.indexOf(code);
+        //  Task: update this code to use your instance variable to return the correct value
+        return this.languageName.get(nameIndex);
     }
 
     /**
@@ -61,8 +73,9 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return language;
+        int codeIndex = this.languageName.indexOf(language);
+        //  Task: update this code to use your instance variable to return the correct value
+        return this.languageCode.get(codeIndex);
     }
 
     /**
@@ -70,7 +83,7 @@ public class LanguageCodeConverter {
      * @return how many languages are included in this code converter.
      */
     public int getNumLanguages() {
-        // TODO Task: update this code to use your instance variable to return the correct value
-        return 0;
+        // Task: update this code to use your instance variable to return the correct value
+        return this.numLanguages;
     }
 }
