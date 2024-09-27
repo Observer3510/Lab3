ab25b3ace8c5a165e077e7a002d9d678052802e1
Annie Szorkin <antonsorkin3@gmail.com>
2024-09-27 12:43:52 -0400

    Finished working on JSONTranslator.java. Tests run well. Did not check tests for completion.

--- src/main/java/org/translation/JSONTranslator.java
+++ src/main/java/org/translation/JSONTranslator.java
@@ -5,21 +5,20 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * An implementation of the Translator interface which reads in the translation
  * data from a JSON file. The data is read in once each time an instance of this class is constructed.
  */
 public class JSONTranslator implements Translator {
-
-    private final JSONArray jsonArray;
-    private final List<String> countries = new ArrayList<>();
-    private final List<String> languages = new ArrayList<>();
-
-    // TODO Task: pick appropriate instance variables for this class
+    private final Map<String, Map<String, String>> countriesToLanguages = new HashMap<>();
 
     /**
      * Constructs a JSONTranslator using data from the sample.json resources file.
@@ -36,18 +35,25 @@ public class JSONTranslator implements Translator {
     public JSONTranslator(String filename) {
         // read the file to get the data to populate things...
         try {
-
             String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource(filename).toURI()));
+            JSONArray jsonArray = new JSONArray(jsonString);
 
-            this.jsonArray = new JSONArray(jsonString);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject countryData = jsonArray.getJSONObject(i);
+                String countryName = countryData.getString("alpha3");
 
-            for (int i = 0; i < this.jsonArray.length(); i++) {
-                assert false;
+                // Create a new map to store translations for this country.
+                Map<String, String> langToTranslation = new HashMap<>();
+                for (String lang: countryData.keySet()) {
+                    // Exclude not language keys: "id", "alpha2", "alpha3".
+                    if (!Arrays.asList("id", "alpha2", "alpha3").contains(lang)) {
+                        String translation = countryData.getString(lang);
+                        langToTranslation.put(lang, translation);
+                    }
+                }
+                // Add this map to the countriesToLanguages.
+                this.countriesToLanguages.put(countryName, langToTranslation);
             }
-
-            // TODO Task: use the data in the jsonArray to populate your instance variables
-            //            Note: this will likely be one of the most substantial pieces of code you write in this lab.
-
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
@@ -56,21 +62,16 @@ public class JSONTranslator implements Translator {
 
     @Override
     public List<String> getCountryLanguages(String country) {
-        // TO_DO Task: return an appropriate list of language codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>(this.languages);
+        return new ArrayList<>(this.countriesToLanguages.get(country).keySet());
     }
 
     @Override
     public List<String> getCountries() {
-        // TO_DO Task: return an appropriate list of country codes,
-        //            but make sure there is no aliasing to a mutable object
-        return new ArrayList<>(this.countries);
+        return new ArrayList<>(this.countriesToLanguages.keySet());
     }
 
     @Override
     public String translate(String country, String language) {
-        // TO_DO Task: complete this method using your instance variables as needed
-        return null;
+        return this.countriesToLanguages.get(country).get(language);
     }
 }
